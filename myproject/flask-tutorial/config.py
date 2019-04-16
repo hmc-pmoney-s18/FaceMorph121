5ce521b0e542c245309858b9485bc8c1b62891cb
Pascal Habineza <pascoball7@WL-213-83.WPA.Claremont.Edu>
2019-04-15 23:17:39 -0700

    restructured the repo

--- myproject/flask-tutorial/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    UPLOAD_FOLDER = os.path.join(os.path.basename('app'), os.path.basename('static'))
